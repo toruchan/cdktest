c1f543dbe0c0187a244a5c0284367fffcf90c1a6
toruchan <ishikawatoru01@gmail.com>
2022-04-28 10:53:20 +0900

    一旦オートスケールのコード削除

--- lib/resources/ecsAsset/albFargateService.ts
+++ lib/resources/ecsAsset/albFargateService.ts
@@ -8,53 +8,51 @@ import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { SslPolicy } from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import { Duration } from "aws-cdk-lib";
 import { CfnLoadBalancer } from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import { Vpc } from "aws-cdk-lib/aws-ec2";
-import { Schedule } from "aws-cdk-lib/aws-events"
+import { Vpc } from "aws-cdk-lib/aws-ec2"
+import * as autoscaling from "aws-cdk-lib/aws-applicationautoscaling"
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch"
 
 export class AlbFargateService extends Resource {
     public service: ApplicationLoadBalancedFargateService
-  
+
     private readonly cluster: Cluster
     private readonly taskDef: FargateTaskDefinition
     private readonly sgGroups: [SecurityGroup]
     private readonly vpc: Vpc
-  
+
     constructor(
-      cluster: Cluster,
-      taskDef: FargateTaskDefinition,
-      sgGroups: [SecurityGroup],
-      vpc: Vpc
+        cluster: Cluster,
+        taskDef: FargateTaskDefinition,
+        sgGroups: [SecurityGroup],
+        vpc: Vpc
     ) {
-      super()
-      this.cluster = cluster
-      this.taskDef = taskDef
-      this.sgGroups = sgGroups
-      this.vpc = vpc
+        super()
+        this.cluster = cluster
+        this.taskDef = taskDef
+        this.sgGroups = sgGroups
+        this.vpc = vpc
     }
-  
     createResources(scope: Construct): void {
         const envType = scope.node.tryGetContext("envType");
-
         const zoneName = scope.node.tryGetContext("zoneName");
         const hostedZoneId = scope.node.tryGetContext("hostedZoneId");
-
         const domainZone = HostedZone.fromHostedZoneAttributes(scope, "host", {
             hostedZoneId,
             zoneName,
         });
-    
+
         // ACM
         const certificate = Certificate.fromCertificateArn(
             scope,
             "Cert",
             scope.node.tryGetContext(`${envType}Certificate`)
         );
-    
+
         let desiredCount = 1
         if (envType == 'staging') {
             desiredCount = 1
         }
-    
+
         this.service = new ApplicationLoadBalancedFargateService(scope, "Service", {
             cluster: this.cluster,
             taskDefinition: this.taskDef,
@@ -74,10 +72,10 @@ export class AlbFargateService extends Resource {
             }),
             healthCheckGracePeriod: Duration.seconds(300),
             circuitBreaker: {
-            rollback: true,
+                rollback: true,
             },
         });
-    
+
         const cfnLoadBalancer = this.service.loadBalancer.node.defaultChild as CfnLoadBalancer
         cfnLoadBalancer.subnets = this.cluster.vpc.selectSubnets({ onePerAz: true, subnetGroupName: 'app-public'}).subnetIds
 
@@ -91,12 +89,12 @@ export class AlbFargateService extends Resource {
             function isFargateService(cdkChild: any): cdkChild is FargateService {
                 return cdkChild instanceof FargateService;
             }
-                
+
             function isCfnService(cdkChild: any): cdkChild is CfnService {
                 return cdkChild instanceof CfnService;
             }
         }
-    
+
         this.service.targetGroup.healthCheck = {
             path: "/",
             healthyHttpCodes: "200",
@@ -104,20 +102,5 @@ export class AlbFargateService extends Resource {
             timeout: Duration.seconds(15),
             interval: Duration.seconds(120),
         };
-
-        const autoScale = this.service.service.autoScaleTaskCount({
-            minCapacity: 1,
-            maxCapacity: 10,
-        });
-
-        autoScale.scaleOnSchedule('PrescaleInTheMorning', {
-            schedule: Schedule.cron({ hour: '18', minute: '0' }),
-            minCapacity: 1,
-        });
-
-        autoScale.scaleOnSchedule('AllowDownscalingAtNight', {
-            schedule: Schedule.cron({ hour: '19', minute: '20' }),
-            minCapacity: 5
-        });
     }
-  }
+}
